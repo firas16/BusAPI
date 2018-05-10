66a771e27306441ca5274c29ba80c840cc8b3684
Unknown <firas.fbs.1989@gmail.com>
2018-05-10 19:15:34 +0200

    add different environments

--- server.py
+++ server.py
@@ -1,10 +1,8 @@
-import os
-
 from app import create_app
 from flask_restful import Api
 from API.bus import Busitem
 
-app = create_app()
+app = create_app('development')
 api = Api(app)
 
 api.add_resource(Busitem, '/bus/<bus_id>')  # Route_3
