6caab15a6c372b4bf00087f038d6011d624b4c79
Unknown <firas.fbs.1989@gmail.com>
2018-05-12 11:22:34 +0200

    Add user model

--- manage.py
+++ manage.py
@@ -2,9 +2,10 @@
 from flask_script import Manager # class for handling a set of commands
 from flask_migrate import Migrate, MigrateCommand
 from app import db, create_app
+from data import user
 from data import bus
 
-app = create_app("testing")
+app = create_app("development")
 migrate = Migrate(app, db)
 manager = Manager(app)
 
