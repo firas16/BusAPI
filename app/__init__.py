47baebc1306f321de0ddb4b4d21bcaf1356f0fc1
Unknown <firas.fbs.1989@gmail.com>
2018-05-12 15:48:22 +0200

    Add authentification

--- app/__init__.py
+++ app/__init__.py
@@ -85,49 +85,8 @@ def create_app(config_name):
             response.status_code = 200
             return response
 
+    # import the authentication blueprint and register it on the app
+    from .auth import auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
     return app
-
-
-# def create_app():
-#     app = Flask(__name__)
-#     app.config['SECRET_KEY'] = '\x91N83\xb3\xea\xdd\x85\xfa\xa1c\xfa\xae\xb1\x1cFFy\xb7i/\xb4\x99\x88'
-#     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/postgres'
-#     db.init_app(app)
-#
-#     from data.bus import Bus
-#
-#     @app.route('/buses/', methods=['GET'])
-#     def buses():
-#         bucketlists = Bus.get_all()
-#         results = []
-#         for bucketlist in bucketlists:
-#             obj = {
-#                 'name': bucketlist.name,
-#             }
-#             results.append(obj)
-#         response = jsonify(results)
-#         response.status_code = 200
-#         return response
-#
-#     @app.route('/bus/', methods=['POST'])
-#     def create_task():
-#         if (request.method == "POST"):
-#             bus = Bus(name='hemla', longitude=10, latitude=25)
-#             bus.save()
-#             response = jsonify({
-#                'id': bus.id,
-#                'name': bus.name,
-#                'longitude': bus.longitude,
-#                'latitude': bus.latitude,
-#
-#             })
-#         return response
-#
-#     from flask import make_response
-#
-#     @app.errorhandler(404)
-#     def not_found(error):
-#         return make_response(jsonify({'error': 'Not found'}), 404)
-#
-#
-#     return app
--- manage.py
+++ manage.py
@@ -4,12 +4,24 @@ from flask_migrate import Migrate, MigrateCommand
 from app import db, create_app
 from data import user
 from data import bus
+import unittest
 
 app = create_app("development")
 migrate = Migrate(app, db)
 manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
+
+# define our command for testing called "test"
+# Usage: python manage.py test
+@manager.command
+def test():
+    """Runs the unit tests without test coverage."""
+    tests = unittest.TestLoader().discover('./tests', pattern='*_suite.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    return 1
 
 if __name__ == '__main__':
     manager.run()
